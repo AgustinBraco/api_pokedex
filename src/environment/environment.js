18bd308668b6abc46f98133e2e988cb29d7b4128
Agustin Braco <bracoagustin@gmail.com>
2025-04-23 01:04:07 -0300

    [Main] - Remove variables not used

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -6,8 +6,6 @@ const environment = {
   PORT: process.env.PORT || 5000,
   ORIGIN_URL: process.env.ORIGIN_URL,
   JWT_SECRET: process.env.JWT_SECRET,
-  USER_EMAIL: process.env.USER_EMAIL,
-  USER_PASSWORD: process.env.USER_PASSWORD,
   ADMIN_EMAIL: process.env.ADMIN_EMAIL,
   ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
   ADMIN_KEY: process.env.ADMIN_KEY,
